39faae34316110cb5b229e1c0addf6066b11b412
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2022-03-24 10:07:36 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+
 
 export default function App() {
+ const baseUrl = 'https://api.jikan.moe/v3/anime/'
+ const [data, setData] = useState([])
+ // const [anime, setAnime] = useState({})
+ 
+ 
+ const randomNumber = () =>{
+   return Math.floor(Math.random() * (400 - 1)) + 1
+ }
+  
+ const animeRandom =  async () => {
+   const req = await axios(`${baseUrl}${randomNumber()}`)
+   console.log(req.data)
+   return req.data;
+ }
+
+const a = animeRandom()
+
+ /*const randomArray = () =>{
+    for(let i = 0; i < 7 ; i++){
+      setData([...data, animeRandom()])
+    }
+ } */
+
+ //useEffect(() =>{
+ // axios.get(`${baseUrl}${randomNumber()}`).then(response => setData( [...data, response.data]))
+
+// },[])
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+         <Text>{JSON.stringify(a)}</Text>
     </View>
   );
 }
