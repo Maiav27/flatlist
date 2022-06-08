677661b3b999a1e0ec6b9cde72b28a6167901237
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2022-06-08 10:06:39 -0300

    'ultimo'

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { render } from 'react-dom';
 
 
 
@@ -9,35 +10,67 @@ import axios from 'axios';
 export default function App() {
  const baseUrl = 'https://api.jikan.moe/v3/anime/'
  const [data, setData] = useState([])
- // const [anime, setAnime] = useState({})
- 
- 
- const randomNumber = () =>{
+
+   const randomNumber = () =>{
    return Math.floor(Math.random() * (400 - 1)) + 1
  }
   
  const animeRandom =  async () => {
-   const req = await axios(`${baseUrl}${randomNumber()}`)
-   console.log(req.data)
-   return req.data;
- }
 
-const a = animeRandom()
+    let req
+  
+    
+      req =  await axios(`${baseUrl}${randomNumber()}`)
 
- /*const randomArray = () =>{
+    
+     
+   return { 
+     id : randomNumber(),
+     titulo : req.data.title,
+     qtdeEpisodios : req.data.episodes,
+     genero : req.data.genres.map((genero, index ) => index + 1 == req.data.genres.length ?  genero.name : genero.name+', '),
+     image : req.data.image_url
+   }
+}
+
+ const randomArray =  async () =>{
+   let array = []
     for(let i = 0; i < 7 ; i++){
-      setData([...data, animeRandom()])
-    }
- } */
+           array[i] =   await animeRandom()
+          
+          }
 
- //useEffect(() =>{
- // axios.get(`${baseUrl}${randomNumber()}`).then(response => setData( [...data, response.data]))
+      Promise.all(array).then(data => setData(data))
+        
+    
+ }  
+ useEffect(() =>{
 
-// },[])
+   randomArray() 
+},[])
+ 
+  
+  const renderItem = ({item}) =>{
+    return(
+
+      <View style={styles.card}>
+         <Image source={ { uri : item.image}} style={styles.image} />
+         <View >
+            <Text> Título : {item.titulo}</Text>
+            <Text> Gênero : {item.genero}</Text>
+            <Text> Quantidade de episodios : {item.qtdeEpisodios}</Text>
+         </View>
+
+      </View>
+
+    )
+  }
+
   
   return (
     <View style={styles.container}>
-         <Text>{JSON.stringify(a)}</Text>
+        <FlatList data={data} renderItem={renderItem} keyExtractor={item => item.id}/>
+      
     </View>
   );
 }
@@ -49,4 +82,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  card : {
+     borderWidth : 2 ,
+     borderRadius : 80,
+     width : 320,
+     height :280, 
+     marginBottom : 10, 
+     borderColor : 'black',
+    
+    
+  },
+  image : {
+    width : 315,
+    height : 150,
+    borderTopRightRadius : 80,
+    borderTopLeftRadius : 80,
+    
+  }
 });
